a180a608433c43c484ed161b64d9388d5ba27f58
adeojoemmanueli <emmanuel@xroads.tech>
2024-12-28 23:40:09 +0100

    feat: updated build

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
             preserveSymlinks: true,
         },
     build: {
-        outDir: "../backend/static",
+        // outDir: "../backend/static",
+        outDir: 'dist',
         emptyOutDir: true,
         sourcemap: true,
         rollupOptions: {
