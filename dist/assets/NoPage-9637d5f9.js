386a88810eff4f0402cf80b9700ac8f0ad448cf1
adeojoemmanueli <emmanuel@xroads.tech>
2024-12-30 02:44:46 +0100

    feat: update versel

---
+++ dist/assets/NoPage-9637d5f9.js
@@ -0,0 +1,2 @@
+import{br as o}from"./vendor-731e06c7.js";import"./fluentui-react-5415e8fa.js";function r(){return o.jsx("h1",{children:"404"})}r.displayName="NoPage";export{r as Component};
+//# sourceMappingURL=NoPage-9637d5f9.js.map
